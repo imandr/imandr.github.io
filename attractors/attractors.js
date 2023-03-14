2aa6065cd057b344377f64e38adc10962bb48ea0
Igor <igorvm@gmail.com>
2023-03-14 09:10:52 -0400

    .

--- attractors/attractors.js
+++ attractors/attractors.js
@@ -452,7 +452,7 @@ class MandelbrotAttractor extends BaseAttractor
         super(np, 
             //[0.3, 0.0], 
             //[0.8, 0.5],
-            [-1.0, -1.0], 
+            [0.1, 0.1], 
             [1.5, 1.5],
             [-1, -1], 
             [1, 1], options);
